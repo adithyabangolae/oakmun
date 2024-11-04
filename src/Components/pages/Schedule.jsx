38e7f5994d62b6721923fa20fe9fdaa0f62f4bb0
adithyabangolae <adithyabangolae2008@gmail.com>
2024-11-04 20:56:34 +0530

    updated schedule

--- src/Components/pages/Schedule.jsx
+++ src/Components/pages/Schedule.jsx
@@ -41,14 +41,13 @@ export default function Schedule() {
 
           <div className="schedule-column">
             <p><strong>Bag Checks:</strong> 7:30 to 8:30</p>
-            <p><strong>Committee Session 4:</strong> 8:30 to 10:00</p>
+            <p><strong>Committee Session 3:</strong> 8:30 to 10:00</p>
             <p><strong>Snack Break:</strong> 10:00 to 10:15</p>
-            <p><strong>Committee Session 5:</strong> 10:15 to 1:30</p>
+            <p><strong>Committee Session 4:</strong> 10:15 to 1:30</p>
             <p><strong>Lunch 1 (IPC, UNHRC, UNSC, AIPPM):</strong> 1:00 to 1:30</p>
             <p><strong>Lunch 2 (UNGA, DISEC, CCC, Comm X):</strong> 1:30 to 2:00</p>
-            <p><strong>Committee Session 6:</strong> 2:00 to 3:45</p>
-            <p><strong>Dispersal:</strong> 3:45 to 4:15</p>
-            <p><strong>Snack Break:</strong> 4:15 to 4:30</p>
+            <p><strong>Committee Session 5:</strong> 2:00 to 4:00</p>
+            <p><strong>Dispersal and Snack Break:</strong> 4:00 to 4:30</p>
             <p><strong>Socials:</strong> 4:30 to 6:30</p>
             <p><strong>Dispersal:</strong> 6:30 to 7:30</p>
           </div>
@@ -67,10 +66,10 @@ export default function Schedule() {
 
           <div className="schedule-column">
             <p><strong>Bag Checks:</strong> 7:30 to 8:30</p>
-            <p><strong>Committee Session 7:</strong> 8:30 to 10:30</p>
-            <p><strong>Snack Break:</strong> 10:30 to 10:45</p>
-            <p><strong>Committee Session 8:</strong> 10:40 to 12:00</p>
-            <p><strong>Closing Ceremony:</strong> 12:00 to 2:00</p>
+            <p><strong>Committee Session 6:</strong> 8:30 to 10:00</p>
+            <p><strong>Snack Break:</strong> 10:00 to 10:15</p>
+            <p><strong>Committee Session 7:</strong> 10:15 to 11:15</p>
+            <p><strong>Closing Ceremony:</strong> 11:30 to 1:15</p>
             <p><strong>Dispersal:</strong> 2:00 to 3:00</p>
           </div>
         </div>
